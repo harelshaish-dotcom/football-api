78a4da6d839648bccf4c3b9b762299b89af54b05
Harel <harel@MacBook-Air-sl-myt.local>
2026-08-29 22:06:01 +0300

    Day 8: Redis setup + startup check

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from database.connection import Base, engine
 from models import league, players, team, user
 from routes import auth, leagues, players as players_routes, teams, users
+from core.cache import r
+
 
 app = FastAPI(title="Football API", version="0.2.0")
+
+@app.on_event("startup")
+async def startup_event():
+    try:
+        await r.ping()
+        print("Redis connected")
+    except Exception as e:
+        print(f"Redis unreachable: {e}")
 
 app.include_router(teams.router)
 app.include_router(users.router)
